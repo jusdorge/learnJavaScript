607c78d34a93e2a448b00aeda1dac55457942ffe
بن حدو محمد أمين <jusdorge@gmail.com>
2019-12-24 17:15:59 +0100

    controllers
    Now we want to control the red square.
    
    Add four buttons, up, down, left, and right.
    
    Write a function for each button to move the component in the selected direction.
    
    Make two new properties in the component constructor, and call them speedX and speedY. These properties are being used as speed indicators.
    
    Add a function in the component constructor, called newPos(), which uses the speedX and speedY properties to change the component's position.
    
    The newpos function is called from the updateGameArea function before drawing the componen

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ function startGame() {
 function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
     this.x = x;
     this.y = y;
     this.update = function(){
@@ -15,10 +17,20 @@ function component(width, height, color, x, y) {
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+    this.newPos = function() {
+      this.x += this.speedX;
+      this.y += this.speedY;
+    }
 }
 function updateGameArea() {
   myGameArea.clear();
-  myGamePiece.x += 1;
+  this.speedX=0;
+  this.speedY=0;
+  if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
+  if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
+  if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
+  if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
+  myGamePiece.newPos();
   myGamePiece.update();
 }
 
@@ -30,6 +42,12 @@ var myGameArea = {
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.interval = setInterval(updateGameArea, 20);
+    window.addEventListener('keydown', function (e) {
+      myGameArea.key = e.keyCode;
+    })
+    window.addEventListener('keyup', function (e) {
+      myGameArea.key = false;
+    })    
   },
   clear : function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
